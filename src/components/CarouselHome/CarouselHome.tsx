6478df1e81b43b7005e22db9ccae532fefa75d87
Camila Citro <97923424+camicitro@users.noreply.github.com>
2023-10-26 18:22:09 -0300

    Update CarouselHome.tsx

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -10,7 +10,7 @@ const CarouselHome = () => {
                     <img 
                     className='d-block w-100'
                     style={{maxHeight: "500px", objectFit: 'cover', height: '100%', objectPosition: 'top'}}
-                    src="src/assets/Images/lemon.avif" alt="" />
+                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -21,7 +21,7 @@ const CarouselHome = () => {
                     <img 
                         className='d-block w-100'
                         style={{maxHeight: "500px", objectFit: 'cover', height: '100%', objectPosition: 'top'}}
-                        src="src/assets/Images/cheesecake.webp" alt="" />
+                        src="https://concepto.de/wp-content/uploads/2019/02/paisaje-natural-portada.jpg" alt="" />
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -31,7 +31,7 @@ const CarouselHome = () => {
                     <img 
                     className='d-block w-100'
                     style={{maxHeight: "500px", objectFit: 'cover', height: '100%', objectPosition: 'top'}}
-                    src="src/assets/Images/carrot.jpg" alt="" />
+                    src="https://www.lavoz.com.ar/resizer/H8uMbuPTZYwFuKpS56PVNtYg7CM=/1200x630/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/3IA6H2MZFNAPLC7KEAMDH42K6M.jpg" alt="" />
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>
